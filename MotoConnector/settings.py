b53a07d182c5365fdbcda63d98a5d8b37eeda737
Sobolevskiy <soboldez77@gmail.com>
2023-12-01 18:43:30 +0300

    allow all hosts for test hosting

--- MotoConnector/settings.py
+++ MotoConnector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@0+^*53*39c$a7=h#k9+263wqniq#724b8l1$gsb65*hwlxhu*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
