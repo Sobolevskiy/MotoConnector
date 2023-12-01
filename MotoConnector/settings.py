fb61e5d5473b858ec80ce21563b765b06e874e17
Sobolevskiy <soboldez77@gmail.com>
2023-12-01 17:16:37 +0300

    Change db and media root

--- MotoConnector/settings.py
+++ MotoConnector/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'MotoConnector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data/db.sqlite3',
     }
 }
 
@@ -131,8 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'data/media')
+MEDIA_URL = 'data/media/'
 
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
